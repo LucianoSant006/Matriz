25db3918507896672f5afda3e5956252a0e16603
LucianoSant006 <santiagoluciano383@gmail.com>
2024-09-12 13:43:33 -0300

    Exec01 Matriz_position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Program {
@@ -7,32 +8,44 @@ public class Program {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		
+
 		int n = sc.nextInt();
-		int[][] mat = new int[n][n];
-		
-		for (int i=0; i<mat.length; i++) {
-			for (int j=0; j<mat[i].length; j++) {
+		int g = sc.nextInt();
+		int[][] mat = new int[n][g];
+
+		for (int i = 0; i < mat.length; i++) {
+
+			for (int j = 0; j < mat[i].length; j++) {
+
 				mat[i][j] = sc.nextInt();
+
 			}
+
 		}
-		
-		System.out.println("Main diagonal:");
-		for (int i=0; i<mat.length; i++) {
-			System.out.print(mat[i][i] + " ");
-		}
-		System.out.println();
-		
-		int count = 0;
+
+		System.out.println("Digite um número da matriz que você quer");
+		sc.nextLine();
+		int x = sc.nextInt();
+
 		for (int i=0; i<mat.length; i++) {
 			for (int j=0; j<mat[i].length; j++) {
-				if (mat[i][j] < 0) {
-					count++;
+				if (mat[i][j] == x) {
+					System.out.println("Position " + i + "," + j + ":");
+					if (j > 0) {
+						System.out.println("Left: " + mat[i][j-1]);
+					}
+					if (i > 0) {
+						System.out.println("Up: " + mat[i-1][j]);
+					}
+					if (j < mat[i].length-1) {
+						System.out.println("Right: " + mat[i][j+1]);
+					}
+					if (i < mat.length-1) {
+						System.out.println("Down: " + mat[i+1][j]);
+					}
 				}
 			}
 		}
-		System.out.println("Negative numbers = " + count);
-		
 		
 		sc.close();
 	}
